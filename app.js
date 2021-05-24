3a08a079293133dacbe3864da5bc57da35958e2a
osbins <arpitjain.2001@gmail.com>
2021-05-24 23:32:00 +0530

    finished weather app with working API

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-let cityName = document.querySelector('#place');
+let cityName = document.querySelector("#place");
+let weather = document.querySelector(".weather");
+let temperature = document.querySelector(".temp");
+let humidity = document.querySelector(".humidity");
+let btn = document.querySelector("#button");
+let API = "695aba4ce78f3ca31491bdfb1e9f6b1e";
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+cityName.value+'&appid=695aba4ce78f3ca31491bdfb1e9f6b1e')
-  .then(response => response.json())
-  .then(data => console.log(data))
+btn.addEventListener("click", function () {
+  // console.log("Clicked");
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName.value +
+      "&units=metric&appid=" +
+      API
+  )
+    .then((smth) => smth.json())
+    .then((lol) => {
+      console.log(lol);
+      weather.innerHTML = lol['weather'][0]['main'];
+      temperature.innerHTML = Math.floor(lol['main']['temp']) + "°C";
+      humidity.innerHTML = lol['main']['humidity'] + "RH";
+    });
+
+});
+
